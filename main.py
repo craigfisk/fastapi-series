e4fbc11d3174972ae57bd8dd00a5644272d1374e
Craig Fisk <craigfisk@marmalade.ai>
2025-11-05 18:54:23 -0500

    At end of #4 at 13:10; now support multiple (chained) query parameters.

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ BANDS = [
 ]
 
 @app.get("/bands")
-async def bands(genre: GenreURLChoices | None = None) -> list[Band]:
+async def bands(genre: GenreURLChoices | None = None,
+    has_albums: bool = False
+) -> list[Band]:
+    band_list = [Band(**b) for b in BANDS]
+
     if genre:
-        return [
-            Band(**b) for b in BANDS if b['genre'].lower() == genre.value
+        band_list = [
+            b for b in band_list if b.genre.lower() == genre.value
         ]
-    return [Band(**b) for b in BANDS]
+
+    if has_albums:
+        band_list = [
+            b for b in band_list if len(b.albums) > 0
+        ]
+    return band_list
 
 @app.get("/bands/{band_id}")
 async def band(band_id: int) -> Band:
