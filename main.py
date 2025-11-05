7238ecd559acf486575de8f39bfdd6890b8e487a
Craig Fisk <craigfisk@marmalade.ai>
2025-11-05 15:25:42 -0500

    In #2, added hard-coded bands list and changed tp @app.get("/bands") as a list[dict].

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def index() -> dict[str, str]:
-    return {'hello': 'world'}
+BANDS = [
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
+    {'id': 3, 'name': 'Black Sabbath', 'genre': 'Metal'},
+    {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},    
+]
+
+@app.get("/bands")
+async def bands() -> list[dict]:
+    return BANDS
 
 @app.get("/about")
 async def about() -> str:
