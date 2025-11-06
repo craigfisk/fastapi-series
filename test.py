a69683a928e856cab2839cb6d4ecdc04bfa4dcab
Craig Fisk <craigfisk@marmalade.ai>
2025-11-06 15:44:45 -0500

    In #5 at 12:45+, added "test.py" which uses python get_type_hints and runs at "python test.py".

---
+++ test.py
@@ -0,0 +1,15 @@
+
+# see https://docs.python.org/3/library/typing.html#typing.get_type_hints
+
+
+from typing import Annotated, get_type_hints
+
+def double(x: Annotated[int, (0,100)]) -> int:
+    type_hints = get_type_hints(double)
+    print(type_hints)
+    return x * 2
+
+result = double(511155) 
+
+print(result)  # This will print 1022230
+
