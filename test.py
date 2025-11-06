69df32e86a7fe00c917c7dfcc36b10eba36ea168
Craig Fisk <craigfisk@marmalade.ai>
2025-11-06 15:47:29 -0500

    In #5 at 14:20, adding "include_extras=true" to "get_type_hints()" and now "python test.py" return includes "0,100" as the range.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@
 from typing import Annotated, get_type_hints
 
 def double(x: Annotated[int, (0,100)]) -> int:
-    type_hints = get_type_hints(double)
+    type_hints = get_type_hints(double, include_extras=True)
     print(type_hints)
     return x * 2
 
