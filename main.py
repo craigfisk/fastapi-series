e52b8fa5555b8308b4b04d20c3e7ed01b3368e09
Craig Fisk <craigfisk@marmalade.ai>
2025-11-05 21:55:37 -0500

    In #5 at 7:30, in main.py added a POST function, still using the hard-coded data.

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ BANDS = [
 @app.get("/bands")
 async def bands(genre: GenreURLChoices | None = None,
     has_albums: bool = False
-) -> list[Band]:
-    band_list = [Band(**b) for b in BANDS]
+) -> list[BandWithID]:
+    band_list = [BandWithID(**b) for b in BANDS]
 
     if genre:
         band_list = [
@@ -32,8 +32,8 @@ async def bands(genre: GenreURLChoices | None = None,
     return band_list
 
 @app.get("/bands/{band_id}")
-async def band(band_id: int) -> Band:
-    band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
+async def band(band_id: int) -> BandWithID:
+    band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
     if band is None:
         raise HTTPException(status_code=404, detail="Band not found")   
     return band
@@ -43,3 +43,10 @@ async def band(band_id: int) -> Band:
 #     return [
 #         b for b in BANDS if b['genre'].lower() == genre.value        
 #     ]
+
+@app.post("/bands")
+async def create_band(band_data: BandCreate) -> BandWithID:
+    id = BANDS[-1]['id'] + 1
+    band = BandWithID(id=id, **band_data.model_dump()).model_dump
+    BANDS.append(band)
+    return band 
