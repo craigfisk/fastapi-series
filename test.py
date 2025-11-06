13bff1c9b4d5d365da37c3a5c3e8173737f05a1d
Craig Fisk <craigfisk@marmalade.ai>
2025-11-06 15:54:08 -0500

    In #5 at 16:04 and getting more info on Annotated: type and value range.

--- test.py
+++ test.py
@@ -2,11 +2,16 @@
 # see https://docs.python.org/3/library/typing.html#typing.get_type_hints
 
 
-from typing import Annotated, get_type_hints
+from typing import Annotated, get_type_hints, get_origin, get_args
 
 def double(x: Annotated[int, (0,100)]) -> int:
     type_hints = get_type_hints(double, include_extras=True)
-    print(type_hints)
+    hint = type_hints['x']
+    if get_origin(hint) is Annotated:
+        hint_type, *hint_args =get_args(hint)
+        print(hint_type)
+        print(hint_args)
+    # print(hint)
     return x * 2
 
 result = double(511155) 
