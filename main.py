b364d64507d61c3d40afdb0d89ae1799dc75f1b2
Craig Fisk <craigfisk@marmalade.ai>
2025-11-07 15:20:21 -0500

    In #7 uncommenting and modifying "bands/{band_id}" in main.py.

--- main.py
+++ main.py
@@ -39,12 +39,12 @@ BANDS = [
 #         ]
 #     return band_list
 
-# @app.get("/bands/{band_id}")
-# async def band(band_id: int) -> BandWithID:
-#     band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
-#     if band is None:
-#         raise HTTPException(status_code=404, detail="Band not found")   
-#     return band
+@app.get("/bands/{band_id}")
+async def band(band_id: Annotated[int, Path(title="The band ID")]) -> Band:
+
+    if band is None:
+        raise HTTPException(status_code=404, detail="Band not found")   
+    return band
 
 @app.post("/bands")
 async def create_band(
