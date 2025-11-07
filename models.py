524349006ca054a29f2f30ae3f1b4c8f24edd7aa
Craig Fisk <craigfisk@marmalade.ai>
2025-11-06 21:42:23 -0500

    Start of #7, "schemas.py" > "models.py", "pip install sqlmodel", and in models.py changed classes Album > AlbumBase and added class Album.

--- models.py
+++ models.py
@@ -1,16 +1,27 @@
 from datetime import date
 from enum import Enum
-from pydantic import BaseModel
-
+from pydantic import BaseModel, model_validator
+from sqlmodel import SQLModel, Field
 
 class GenreURLChoices(Enum):
     ROCK = "rock"
     ELECTRONIC = "electronic"
     METAL = "metal"
     HIP_HOP = "hip-hop"
-class Album(BaseModel):
+
+class GenreChoices(Enum):
+    ROCK = "Rock"
+    ELECTRONIC = "Electronic"
+    METAL = "Metal"
+    HIP_HOP = "Hip-Hop"
+
+class AlbumBase(SQLModel):
     title: str
     release_date: date
+
+class Album(AlbumBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    
 class BandBase(BaseModel):
     # id: int
     name: str
