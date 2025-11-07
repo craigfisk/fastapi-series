8465d81de5d59eecd621dc2d35da87f5c486713f
Craig Fisk <craigfisk@marmalade.ai>
2025-11-07 15:15:11 -0500

    In #7 at 19:10, but "Send Request" in "api.http" does not seem right, if there are albums.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path, Query, Depends
 from models import GenreURLChoices, BandBase, BandCreate, Band, Album
 from typing import Annotated
+from sqlmodel import Session
 from contextlib import asynccontextmanager
-from db import init_db
+from db import init_db, get_session
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -45,9 +46,22 @@ BANDS = [
 #         raise HTTPException(status_code=404, detail="Band not found")   
 #     return band
 
-# @app.post("/bands")
-# async def create_band(band_data: BandCreate) -> BandWithID:
-#     id = BANDS[-1]['id'] + 1
-#     band = BandWithID(id=id, **band_data.model_dump()).model_dump()
-#     BANDS.append(band)
-#     return band 
+@app.post("/bands")
+async def create_band(
+    band_data: BandCreate,
+    session: Session = Depends(get_session)
+) -> Band:
+    band = Band(name=band_data.name, genre=band_data.genre)
+    session.add(band)
+
+    if band_data.albums:
+        for album in band_data.albums:
+            album_obj = Album(
+                title=album.title,
+                release_date=album.release_date,
+                band=band
+            )
+            session.add(album_obj)
+    session.commit()
+    session.refresh(band)
+    return band 
