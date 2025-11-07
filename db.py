1b8d592447b545ea01234b325e93876a0010bb71
Craig Fisk <craigfisk@marmalade.ai>
2025-11-07 14:50:05 -0500

    In #7 at 11:45, added "db.py" which creates SQLite database and modified "main.py" to do that.

---
+++ db.py
@@ -0,0 +1,10 @@
+from sqlmodel import create_engine, SQLModel, Session
+
+DATABASE_URL = "sqlite:///db.sqlite"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+    
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from models import GenreURLChoices, BandBase, BandCreate, Band, Album
+from typing import Annotated
+from contextlib import asynccontextmanager
+from db import init_db
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
 
-
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 BANDS = [
     {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
@@ -15,39 +21,33 @@ BANDS = [
     {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},    
 ]
 
-@app.get("/bands")
-async def bands(genre: GenreURLChoices | None = None,
-    has_albums: bool = False
-) -> list[BandWithID]:
-    band_list = [BandWithID(**b) for b in BANDS]
+# @app.get("/bands")
+# async def bands(genre: GenreURLChoices | None = None,
+#     has_albums: bool = False
+# ) -> list[BandWithID]:
+#     band_list = [BandWithID(**b) for b in BANDS]
 
-    if genre:
-        band_list = [
-            b for b in band_list if b.genre.lower() == genre.value
-        ]
+#     if genre:
+#         band_list = [
+#             b for b in band_list if b.genre.lower() == genre.value
+#         ]
 
-    if has_albums:
-        band_list = [
-            b for b in band_list if len(b.albums) > 0
-        ]
-    return band_list
+#     if has_albums:
+#         band_list = [
+#             b for b in band_list if len(b.albums) > 0
+#         ]
+#     return band_list
 
-@app.get("/bands/{band_id}")
-async def band(band_id: int) -> BandWithID:
-    band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
-    if band is None:
-        raise HTTPException(status_code=404, detail="Band not found")   
-    return band
+# @app.get("/bands/{band_id}")
+# async def band(band_id: int) -> BandWithID:
+#     band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
+#     if band is None:
+#         raise HTTPException(status_code=404, detail="Band not found")   
+#     return band
 
-# @app.get('/bands/genre/{genre}')
-# async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:
-#     return [
-#         b for b in BANDS if b['genre'].lower() == genre.value        
-#     ]
-
-@app.post("/bands")
-async def create_band(band_data: BandCreate) -> BandWithID:
-    id = BANDS[-1]['id'] + 1
-    band = BandWithID(id=id, **band_data.model_dump()).model_dump()
-    BANDS.append(band)
-    return band 
+# @app.post("/bands")
+# async def create_band(band_data: BandCreate) -> BandWithID:
+#     id = BANDS[-1]['id'] + 1
+#     band = BandWithID(id=id, **band_data.model_dump()).model_dump()
+#     BANDS.append(band)
+#     return band 
